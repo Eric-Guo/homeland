8db570322f8cfd0bee88a6494939ea4955f8e949
Eric Guo <eric.guocz@gmail.com>
2022-09-01 17:06:12 +0800

    Enable SSL for all.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-set :nginx_use_ssl, false
+set :nginx_use_ssl, true
 set :rails_env, 'production'
 set :puma_service_unit_name, :puma_thape_forum
 set :puma_systemctl_user, :system
